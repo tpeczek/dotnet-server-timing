0a3a5d6c167a486ee675f71512876d78e7351bf2
Tomasz Pęczek <tpeczek@gmail.com>
2021-12-16 22:57:50 +0100

    Adding basic demo

---
+++ Demo.AspNetCore.ServerTiming/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.AspNetCore.ServerTiming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConsole();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ Demo.AspNetCore.ServerTiming/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Lib.AspNetCore.ServerTiming;
+using Lib.AspNetCore.ServerTiming.Http.Headers;
+using Microsoft.Extensions.Hosting;
+
+namespace Demo.AspNetCore.ServerTiming
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServerTiming();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseServerTiming("http://localhost:8000", "http://localhost:8001")
+                .Run(async (context) =>
+                {
+                    IServerTiming serverTiming = context.RequestServices.GetRequiredService<IServerTiming>();
+                    serverTiming.Metrics.Add(new ServerTimingMetric("cache", 300, "Cache"));
+                    serverTiming.Metrics.Add(new ServerTimingMetric("sql", 900, "Sql Server"));
+                    serverTiming.Metrics.Add(new ServerTimingMetric("fs", 600, "FileSystem"));
+                    serverTiming.Metrics.Add(new ServerTimingMetric("cpu", 1230, "Total CPU"));
+
+                    await context.Response.WriteAsync("-- Demo.AspNetCore.ServerTiming --");
+                });
+        }
+    }
+}
